4988293d28703e3a9f72e3f9cb9208861a6a048e
kimleang <leangk49@gmail.com>
2024-03-11 18:25:20 +0700

    user column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar')->default('/uploads/default-avatar.png');
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role',['user','admin'])->default('user');  //enum = enumerate
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
